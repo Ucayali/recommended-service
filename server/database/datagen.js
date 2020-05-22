1885df93fad24eefde65682ec23cc8cccafc5395
Adam Tiller <a.j.tiller@gmail.com>
2020-05-22 16:54:51 -0700

    Add data generation script for main csv.

---
+++ server/database/datagen.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+const faker = require('faker');
+
+const writer = csvWriter();
+
+const TOTAL = 10000000;
+let idCounter = 1;
+
+function imageFile() {
+  return Math.ceil(Math.random() * 988).toString().padStart(4, '0');
+}
+
+function makeLine(id) {
+  const obj = {};
+  const numRecommended = 20 + Math.ceil(Math.random() * 30);
+
+  obj.id = id;
+  obj.productId = id;
+  obj.name = faker.commerce.productName();
+  obj.price = faker.commerce.price(5.00, 75.00, 2);
+  obj.imageUrl = `http://ds6fr0ymcho38.cloudfront.net/${imageFile()}.jpg`;
+  obj.numReviews = Math.ceil(Math.random() * 250);
+  obj.avgRating = Math.ceil(Math.random() * 5);
+
+  for (let i = 0; i < numRecommended; i += 1) {
+    obj[i] = Math.ceil(Math.random() * 10000000);
+  }
+
+  return obj;
+}
+
+function done() {
+  console.log(`Added ${idCounter} rows!`);
+  writer.end();
+}
+
+function write() {
+  let ok = true;
+
+  while (idCounter <= TOTAL && ok) {
+    idCounter += 1;
+
+    if (idCounter === TOTAL) {
+      writer.write(makeLine(idCounter), done);
+    } else {
+      ok = writer.write(makeLine(idCounter));
+    }
+  }
+
+  if (idCounter < TOTAL) {
+    writer.once('drain', write);
+  }
+}
+
+writer.pipe(fs.createWriteStream('data.csv'));
+
+write();
